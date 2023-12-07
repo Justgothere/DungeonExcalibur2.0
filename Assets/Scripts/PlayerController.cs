1b6c893787e6b022ef239065c74ea19fd4578b45
Justgothere <90232864+Justgothere@users.noreply.github.com>
2023-12-07 12:48:13 -0500

    Added scripts and tiles
    Scripts and tiles added in. Game palette is there but everything is a collider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 10;
+
+    public int health { get { return currentHealth; } }
+
+    int currentHealth;
+
+    public float timeInvincible = 1.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2D;
+    float horizontal;
+    float vertical;
+
+    Animator animator;
+
+    Vector2 lookDirection = new Vector2(1, 0);
+
+
     // Start is called before the first frame update
     void Start()
     {
+        currentHealth = maxHealth;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2D.MovePosition(position);
     }
 }
