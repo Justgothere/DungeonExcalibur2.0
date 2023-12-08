d7abdb5210807cae947ed1c7288cc1ab268fe757
hannahnarjes <hannahnarjes@outlook.com>
2023-12-08 14:39:10 -0600

    more jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
 
     void Update()
     {
         // Get input values for horizontal and vertical axes
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
 
         // Calculate movement vector
-        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+        Vector2 movement = new Vector2(horizontalInput, 0f);
 
         // Normalize the vector to prevent faster diagonal movement
         movement = movement.normalized;
 
         // Move the character
         MoveCharacter(movement);
+
+        // Check if the player is grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f, groundLayer);
+
+        // Jump when the space key is pressed and the player is grounded
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
     }
 
     void MoveCharacter(Vector2 direction)
@@ -25,34 +43,9 @@ public class PlayerController : MonoBehaviour
         // Move the character using Rigidbody2D
         transform.Translate(direction * speed * Time.deltaTime);
     }
-
-
-
-
-    void FixedUpdate()
+    void Jump()
     {
-        MovePlayer();
-    }
-
-    void MovePlayer()
-    {
-        // Apply gravity
-        Vector2 gravityForce = new Vector2(0, -9.8f) * gravityScale; // Adjust -9.8f based on your gravity needs
-        GetComponent<Rigidbody2D>().AddForce(gravityForce);
-
-        Vector2 moveDirection = new Vector2(horizontal, vertical).normalized;
-
-        if (moveDirection != Vector2.zero)
-        {
-            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-            GetComponent<Rigidbody2D>().rotation = angle;
-        }
-
-        Vector2 position = GetComponent<Rigidbody2D>().position;
-        position.x += speed * horizontal * Time.fixedDeltaTime;
-        //position.y += speed * vertical * Time.fixedDeltaTime;
-
-        GetComponent<Rigidbody2D>().MovePosition(position);
+        // Add an upward force to the rigidbody to make the character jump
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
-
