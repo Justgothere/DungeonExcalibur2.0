dd2832e206ce0646ac09f9403fad0b849feb51aa
Justgothere <90232864+Justgothere@users.noreply.github.com>
2023-12-08 15:36:30 -0500

    Fixed?
    Fixed compiling error?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,34 +25,4 @@ public class PlayerController : MonoBehaviour
         // Move the character using Rigidbody2D
         transform.Translate(direction * speed * Time.deltaTime);
     }
-
-
-
-
-    void FixedUpdate()
-    {
-        MovePlayer();
-    }
-
-    void MovePlayer()
-    {
-        // Apply gravity
-        Vector2 gravityForce = new Vector2(0, -9.8f) * gravityScale; // Adjust -9.8f based on your gravity needs
-        GetComponent<Rigidbody2D>().AddForce(gravityForce);
-
-        Vector2 moveDirection = new Vector2(horizontal, vertical).normalized;
-
-        if (moveDirection != Vector2.zero)
-        {
-            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-            GetComponent<Rigidbody2D>().rotation = angle;
-        }
-
-        Vector2 position = GetComponent<Rigidbody2D>().position;
-        position.x += speed * horizontal * Time.fixedDeltaTime;
-        //position.y += speed * vertical * Time.fixedDeltaTime;
-
-        GetComponent<Rigidbody2D>().MovePosition(position);
-    }
 }
-
