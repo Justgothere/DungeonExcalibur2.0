05983d1a3bf80d66ee46b972bd620939a213c60e
hannahnarjes <hannahnarjes@outlook.com>
2023-12-08 11:59:15 -0600

    player movement code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 3.0f;
-
+    public float gravityScale = 1.0f;
     public int maxHealth = 10;
 
     public int health { get { return currentHealth; } }
@@ -29,22 +29,15 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.gravityScale = gravityScale;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
+        HandleMovementInput();
 
         if (isInvincible)
         {
@@ -53,14 +46,43 @@ public class PlayerController : MonoBehaviour
                 isInvincible = false;
         }
     }
+    void HandleMovementInput()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical).normalized;
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection = move;
+        }
+    }
 
 
 
     void FixedUpdate()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        // Apply gravity
+        Vector2 gravityForce = new Vector2(0, -9.8f) * gravityScale; // Adjust -9.8f based on your gravity needs
+        rigidbody2D.AddForce(gravityForce);
+
+        Vector2 moveDirection = new Vector2(horizontal, vertical).normalized;
+
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            rigidbody2D.rotation = angle;
+        }
+
         Vector2 position = rigidbody2D.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        position.x += speed * horizontal * Time.fixedDeltaTime;
+        position.y += speed * vertical * Time.fixedDeltaTime;
 
         rigidbody2D.MovePosition(position);
     }
