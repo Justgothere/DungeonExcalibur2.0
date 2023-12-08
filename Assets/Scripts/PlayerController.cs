b8a0d4e5f86afe6473c2a19a9b5f56bb6f1b3864
hannahnarjes <hannahnarjes@outlook.com>
2023-12-08 14:10:52 -0600

    player movement scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,89 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 3.0f;
-    public float gravityScale = 1.0f;
-    public int maxHealth = 10;
+    public float speed = 5f;
 
-    public int health { get { return currentHealth; } }
-
-    int currentHealth;
-
-    public float timeInvincible = 1.0f;
-    bool isInvincible;
-    float invincibleTimer;
-
-    Rigidbody2D rigidbody2D;
-    float horizontal;
-    float vertical;
-
-    Animator animator;
-
-    Vector2 lookDirection = new Vector2(1, 0);
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentHealth = maxHealth;
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        rigidbody2D.gravityScale = gravityScale;
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        HandleMovementInput();
+        // Get input values for horizontal and vertical axes
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        if (isInvincible)
-        {
-            invincibleTimer -= Time.deltaTime;
-            if (invincibleTimer < 0)
-                isInvincible = false;
-        }
-    }
-    void HandleMovementInput()
-    {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        // Calculate movement vector
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
 
-        Vector2 move = new Vector2(horizontal, vertical).normalized;
+        // Normalize the vector to prevent faster diagonal movement
+        movement = movement.normalized;
 
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection = move;
-        }
+        // Move the character
+        MoveCharacter(movement);
     }
 
-
-
-    void FixedUpdate()
+    void MoveCharacter(Vector2 direction)
     {
-        MovePlayer();
-    }
-
-    void MovePlayer()
-    {
-        // Apply gravity
-        Vector2 gravityForce = new Vector2(0, -9.8f) * gravityScale; // Adjust -9.8f based on your gravity needs
-        rigidbody2D.AddForce(gravityForce);
-
-        Vector2 moveDirection = new Vector2(horizontal, vertical).normalized;
-
-        if (moveDirection != Vector2.zero)
-        {
-            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-            rigidbody2D.rotation = angle;
-        }
-
-        Vector2 position = rigidbody2D.position;
-        position.x += speed * horizontal * Time.fixedDeltaTime;
-        position.y += speed * vertical * Time.fixedDeltaTime;
-
-        rigidbody2D.MovePosition(position);
+        // Move the character using Rigidbody2D
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 }
